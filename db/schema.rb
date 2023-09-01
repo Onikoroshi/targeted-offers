ea2c4231c5bd7067f85fd3b0f25fb0448774cc58
Peter Cormack <petertcormack@gmail.com>
2023-09-01 11:26:04 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_01_031923) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_01_182345) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "gender_offer_criteria", force: :cascade do |t|
+    t.bigint "offer_criterion_id", null: false
+    t.bigint "gender_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["gender_id"], name: "index_gender_offer_criteria_on_gender_id"
+    t.index ["offer_criterion_id"], name: "index_gender_offer_criteria_on_offer_criterion_id"
+  end
+
   create_table "genders", force: :cascade do |t|
     t.string "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "offer_criteria", force: :cascade do |t|
+    t.bigint "offer_id", null: false
+    t.integer "min_age"
+    t.integer "max_age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["offer_id"], name: "index_offer_criteria_on_offer_id"
+  end
+
+  create_table "offers", force: :cascade do |t|
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -39,4 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_01_031923) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "gender_offer_criteria", "genders"
+  add_foreign_key "gender_offer_criteria", "offer_criteria"
+  add_foreign_key "offer_criteria", "offers"
 end
