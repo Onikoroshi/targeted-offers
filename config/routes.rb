83811fee6522bfd2cd5a9448b28c11a5c783ae72
Peter Cormack <petertcormack@gmail.com>
2023-09-01 11:17:12 -0700

    add offers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :genders
   devise_for :users
   authenticated :user do
     root "offers#index", as: :authenticated_root
   end
 
   root 'landing_page#index'
+
+  resources :offers, only: :index
+  # resources :genders # TODO: admin page to manage
 end
